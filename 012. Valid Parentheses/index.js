aac5942a9185c71520e4f0c409170b9905e465f9
Himanshu Digari <himanshudigari@gmail.com>
2023-09-12 23:36:43 +0530

    Valid Parentheses: Solution Implemented

---
+++ 012. Valid Parentheses/index.js
@@ -0,0 +1,21 @@
+// **Valid Parentheses
+
+let _isValidBracketString = function(s) {
+    const bracketStack = [];
+    const bracketMapping = new Map([
+        [')', '('],
+        ['}', '{'],
+        [']', '[']
+    ]);
+
+    for (const bracket of s) {
+        if (bracket === '(' || bracket === '{' || bracket === '[')
+            bracketStack.push(bracket);
+        else if (bracketStack.length === 0 || bracketMapping.get(bracket) !== bracketStack.pop()) return false
+    }
+
+    return bracketStack.length === 0;
+};
+
+let str = '()[()]{{}()}';
+console.log(`'${str}' - This bracket string is`, _isValidBracketString(str) ? 'valid.': 'invalid.');
